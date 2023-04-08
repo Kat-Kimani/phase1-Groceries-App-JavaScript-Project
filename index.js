745faf5407c0741056607e624179f0ae9d0ce4bb
catherine <wachukimani@gmail.com>
2023-04-08 23:44:59 +0300

    sorted out the bug

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ document.addEventListener("DOMContentLoaded", () => {
     <h4 id="product-name">${item.name}</h4>
     <p id="veges"> Veges: ${item.vegetable} </p>
     <p id="fruit"> Fruit: ${item.fruit} </p>
-     <p id="description">${item.description} </p>
+    <p id="description">${item.description} </p>
     <p id="price"> Price: ${item.price} </p>
-    <div id="order-count">0</div>
+    <div id="order-count${item.id}">0</div>
     <button id="buy-basket${item.id}" class="btn btn-primary">Buy Basket
     </button>
     </div>
@@ -41,71 +41,132 @@ document.addEventListener("DOMContentLoaded", () => {
             //console.log("clicked")
             order++;
             console.log(`Orders: ${order}`);
-            document.querySelector("#order-count").textContent = order;
+            const orderCount = document.querySelector(`#order-count${item.id}`);
+            orderCount.textContent = parseInt(orderCount.textContent) + 1;
           });
         });
       });
-
-    const customOrderForm = document.querySelector("form");
-
-    function getInputData(event) {
-      event.preventDefault(); // Prevent default form submission behavior
-      // Get input data from form
-      let name = document.getElementById("name").value;
-      let order = document.getElementById("textBox").value;
-
-      // Reset form
-      document.getElementById("name").value = "";
-      document.getElementById("textBox").value = "";
-
-      // Pass order data to the placeOrder function
-      placeOrder({ name: name, order: order });
-    }
-
-    customOrderForm.addEventListener("submit", getInputData);
-
-    //POST METHOD used to add an order through the order form
-    function placeOrder(order) {
-      fetch("http://localhost:3000/groceries", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        body: JSON.stringify(order),
-      }).then(resp);
-      //=> resp.json())
-      // .then((order) => console.log(groceries));
-    }
-    function searchItems() {
-      const searchValue = document.getElementById("search").value.toLowerCase();
-      const cards = document.querySelectorAll(".card");
-
-      cards.forEach((card) => {
-        const name = card
-          .querySelector("#product-name")
-          .textContent.toLowerCase();
-        const vegetable = card
-          .querySelector("#veges")
-          .textContent.toLowerCase();
-        const fruit = card.querySelector("#fruit").textContent.toLowerCase();
-
-        if (
-          name.includes(searchValue) ||
-          vegetable.includes(searchValue) ||
-          fruit.includes(searchValue)
-        ) {
-          card.style.display = "block";
-        } else {
-          card.style.display = "none";
-        }
-      });
-    }
-
-    const searchBtn = document.getElementById("search-btn");
-    searchBtn.addEventListener("click", searchItems);
   }
 
   let order = 0;
   displayGroceries();
 });
+
+const customOrderForm = document.querySelector("#create-form");
+
+function getInputData(event) {
+  console.log("getInputData called");
+  event.preventDefault(); // Prevent default form submission behavior
+  console.log(document.getElementById("product-name").value);
+
+  // Get input data from form
+  const addImage = document.getElementById("image-url").value;
+  const addName = document.getElementById("product-names").value;
+  const addVeges = document.getElementById("vegez").value;
+  const addFruit = document.getElementById("fruits").value;
+  const addPrice = document.getElementById("prices").value;
+  const addDescription = document.getElementById("descriptions").value;
+
+  // Pass order data to the placeOrder function
+  placeOrder({
+    image: addImage,
+    name: addName,
+    vegetable: addVeges,
+    fruit: addFruit,
+    price: addPrice,
+    description: addDescription,
+  });
+
+  // Reset form
+  document.getElementById("image-url").value = " ";
+  document.getElementById("product-names").value = " ";
+  document.getElementById("vegez").value = "";
+  document.getElementById("fruits").value = "";
+  document.getElementById("prices").value = "";
+  document.getElementById("descriptions").value = "";
+}
+
+customOrderForm.addEventListener("submit", getInputData);
+
+//POST METHOD used to add an order through the order form
+function placeOrder(order) {
+  console.log("order:", order);
+  fetch("http://localhost:3000/groceries", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+
+    body: JSON.stringify(order),
+  })
+    .then((resp) => resp.json())
+    .then((grocery) => console.log(grocery));
+}
+// Define updateBasket function to edit
+function updateBasket(basketObj) {
+  fetch(`http://localhost:3000/groceries/${item.name}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ basketObj }),
+  })
+    .then((resp) => resp.json())
+    .then((grocery) => console.log(grocery));
+}
+
+// Add event listener for each "Update Basket" button
+const editBtn = document.querySelector("#edit-button");
+editBtn.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    // Retrieve the item ID and form inputs
+    const itemId = btn.getAttribute("data-id");
+    const imageUrl = document.querySelector("#image-url").value;
+    const productName = document.querySelector("#product-names").value;
+    const vegetables = document.querySelector("#vegez").value;
+    const fruits = document.querySelector("#fruits").value;
+    const price = document.querySelector("#prices").value;
+    const description = document.querySelector("#descriptions").value;
+
+    //const itemId = btn.getAttribute("data-id");
+
+    // Construct the updated basket object
+    const basketObj = {
+      imageUrl,
+      productName,
+      vegetables,
+      fruits,
+      price,
+      description,
+    };
+
+    updateBasket(basketObj);
+  });
+});
+
+function searchItems() {
+  const searchValue = document.getElementById("search").value.toLowerCase();
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    const name = card.querySelector("#product-name").textContent.toLowerCase();
+    const vegetable = card.querySelector("#veges").textContent.toLowerCase();
+    const fruit = card.querySelector("#fruit").textContent.toLowerCase();
+
+    if (
+      name.includes(searchValue) ||
+      vegetable.includes(searchValue) ||
+      fruit.includes(searchValue)
+    ) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+}
+
+const searchBtn = document.getElementById("search-btn");
+searchBtn.addEventListener("click", searchItems);
